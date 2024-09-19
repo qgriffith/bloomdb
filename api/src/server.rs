2ce58d0902b0198313c4db1207820d616f3f9929
qgriffith <qgriffith@gmail.com>
2024-09-19 16:43:27 -0400

    Enhance middleware with error handling and tracing
    Added `HandleErrorLayer` for improved error management and `TraceLayer` for request tracing. Introduced request timeouts to prevent long waits and updated dependencies to support these features.

--- api/src/server.rs
+++ api/src/server.rs
@@ -1,9 +1,18 @@
-use axum::{response::{Html, IntoResponse}, routing::get, Router, http::StatusCode};
+use axum::{
+    http::StatusCode,
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
+};
 
+use crate::roasts;
+use axum::error_handling::HandleErrorLayer;
 use sea_orm::Database;
 use std::env;
-use tower_cookies::{CookieManagerLayer, Cookies};
-use crate::roasts;
+use std::time::Duration;
+use tower::{BoxError, ServiceBuilder};
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn start() -> anyhow::Result<()> {
@@ -23,13 +32,29 @@ async fn start() -> anyhow::Result<()> {
     let app = Router::new()
         .route("/", get(handler))
         .route("/api/roast", get(roasts::get_roasts))
-        .layer(CookieManagerLayer::new())
+        // Add middleware to all routes
+        .layer(
+            ServiceBuilder::new()
+                .layer(HandleErrorLayer::new(|error: BoxError| async move {
+                    if error.is::<tower::timeout::error::Elapsed>() {
+                        Ok(StatusCode::REQUEST_TIMEOUT)
+                    } else {
+                        Err((
+                            StatusCode::INTERNAL_SERVER_ERROR,
+                            format!("Unhandled internal error: {error}"),
+                        ))
+                    }
+                }))
+                .timeout(Duration::from_secs(30))
+                .layer(TraceLayer::new_for_http())
+                .into_inner(),
+        )
         .with_state(conn);
 
     let app = app.fallback(handler_404);
 
     let listener = tokio::net::TcpListener::bind(&server_url).await?;
-    tracing::debug!("listening on {}", listener.local_addr().unwrap());;
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await?;
 
     Ok(())
