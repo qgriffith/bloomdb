7ad48d76db74e69524efb612c6cad6f5e85f28c8
qgriffith <qgriffith@gmail.com>
2024-09-19 06:29:27 -0400

    Add initial data to brewers table
    This commit updates the `m20240918_164352_create_brewers.rs` migration to insert initial data into the brewers table. The insert statements add multiple brewer types including "Omni Dripper v2", "xPod", "Aeropress", "Omni Dripper v1", and "Other".

--- migration/src/m20240918_164352_create_brewers.rs
+++ migration/src/m20240918_164352_create_brewers.rs
@@ -6,7 +6,6 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-
         manager
             .create_table(
                 Table::create()
@@ -16,11 +15,22 @@ impl MigrationTrait for Migration {
                     .col(string(Brewer::Type))
                     .to_owned(),
             )
-            .await
+            .await?;
+
+        let insert = Query::insert()
+            .into_table(Brewer::Table)
+            .columns([Brewer::Type])
+            .values_panic(["Omni Dripper v2".into()])
+            .values_panic(["xPod".into()])
+            .values_panic(["Aeropress".into()])
+            .values_panic(["Omni Dripper v1".into()])
+            .values_panic(["Other".into()])
+            .to_owned();
+        manager.exec_stmt(insert).await?;
+        Ok(())
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-
         manager
             .drop_table(Table::drop().table(Brewer::Table).to_owned())
             .await
